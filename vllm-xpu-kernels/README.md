344146dae3ddfc87d16800abb1cba031638c3f99
thegreatchaos <zhenchao.liu@intel.com>
2026-05-27 17:20:44 +0800

    update

---
+++ vllm-xpu-kernels/README.md
@@ -0,0 +1,6 @@
+## HOWTO
+```
+setupEnv.sh #to setup the env
+source env.sh # to set envs
+build.sh #to build vllm-xpu-kernels, check it for detailed options
+```
--- vllm-xpu-kernels/env.sh
+++ vllm-xpu-kernels/env.sh
@@ -1,4 +1,4 @@
-export ROOT="$(cd "$(dirname "$0")" && pwd)"
+export ROOT="$(cd $(dirname "$1") && pwd)"
 source /opt/intel/oneapi/setvars.sh
 source .env/bin/activate
 export PS1='>'
