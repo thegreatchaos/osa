07b480ca5fc2b55bc4d9861bb318eeb2a3f966ca
thegreatchaos <zhenchao.liu@intel.com>
2026-05-29 13:46:20 +0800

    update

--- vllm-xpu-kernels/setupEnv.sh
+++ vllm-xpu-kernels/setupEnv.sh
@@ -1,24 +1,22 @@
+export ROOT="$(cd $(dirname "$1") && pwd)"
 set -x
 gs(){ #get source
+    cd ${ROOT}
     if [ ! -d "src" ]; then
 	git submodule add --force https://github.com/vllm-project/vllm-xpu-kernels.git src
-	cd src;
-	git checkout v0.1.7
     fi
     if [ ! -d "app.src" ]; then
 	git submodule add --force https://github.com/intel-innersource/applications.ai.gpu.vllm-xpu app.src
-	cd app.src
-	git checkout 58625a191b2367dba56386065a35e9599a56c20f
     fi
 }
 pei(){ #python env init
+    cd ${ROOT}
     if [ ! -d ".env" ]; then
 	python3.12 -m venv .env
+	source env.sh
+	pip install --upgrade pip
     fi;
     source env.sh
-    if [ ! -d ".env" ]; then
-	pip install --upgrade pip
-    fi
 }
 xpuDeps(){
     pip install -r ${ROOT}/src/requirements.txt
@@ -26,8 +24,6 @@ xpuDeps(){
 
 ## for xpu kernels
 kerns(){
-    gs;
-    pei;
     xpuDeps;
     export VLLM_TARGET_DEVICE="xpu"
     export CMAKE_BUILD_TYPE="Release" #在ptl 64GB + 即便128GB swap上也无法build debug信息的
@@ -43,17 +39,15 @@ kerns(){
 
 ## for https://github.com/intel-innersource/applications.ai.gpu.vllm-xpu
 app(){
-    gs;
     cd ${ROOT}/app.src
     pip install -r requirements/xpu.txt
     VLLM_TARGET_DEVICE=xpu pip install --no-build-isolation -e . -v
-    #pip uninstall vllm-xpu-kernels
     pip install triton-xpu==3.7.0 --extra-index-url https://download.pytorch.org/whl/xpu
 }
 
 
 
-
+gs;
 pei;
 app;
 kerns;
