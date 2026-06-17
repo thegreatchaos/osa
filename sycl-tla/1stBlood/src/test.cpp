bd61ce9af44edf95ba31d87d97e8509f01127069
thegreatchaos@ptldev <zhenchao.liu@intel.com>
2026-06-17 16:48:59 +0000

    switch the example to MHA, WIP: rampup SYCL-TLA with MHA

--- sycl-tla/1stBlood/src/test.cpp
+++ sycl-tla/1stBlood/src/test.cpp
@@ -1,197 +1,11 @@
-#include "cutlass/epilogue/collective/default_epilogue.hpp"
-#include "cutlass/epilogue/collective/xe_epilogue.hpp"
-#include "cutlass/epilogue/fusion/xe_callbacks.hpp"
-#include "cutlass/gemm/device/gemm_universal.h"
-#include "cutlass/gemm/device/gemm_universal_adapter.h"
-#include "cutlass/gemm/collective/collective_mma.hpp"
-#include "cutlass/util/GPU_Clock.hpp"
-#include <cute/tensor.hpp>
-#include <random>
-#include "cutlass/util/command_line.h"
-#include "cutlass/util/device_memory.h"
-#include "cutlass/util/packed_stride.hpp"
-#include "cutlass/util/reference/device/gemm_complex.h"
-#include "cutlass/util/reference/device/tensor_compare.h"
-#include "helper.h"
-#include "cutlass/cutlass.h"
-#include "cutlass/util/device_memory.h"
-#include "cutlass/util/reference/device/sycl_tensor_fill.h"
-#include "cutlass/util/initialize_block.hpp"
-
-template<typename T>
-inline
-bool is_close(T a, T b, float atol, float rtol) {
-  return std::abs((float)a - (float)b) <= atol + rtol * std::abs((float)b);
-}
-
-template <class, class, class> class convert_dtype_name;
-
-template <typename SrcT, typename DstT, typename Runner>
-void convert_dtype(const SrcT* d_src, DstT* d_dst, size_t size) {
-  compat::get_default_queue().parallel_for<convert_dtype_name<SrcT, DstT, Runner>>(size, [=](auto indx) {
-    d_dst[indx] = static_cast<DstT>(d_src[indx]);
-  }).wait();
-}
-using namespace cute;
-struct Options {
-  bool help;
-  bool error;
-  int m, n, k, l, iterations, verify;
-  float alpha, beta;
-  Options():
-    help(false),
-    error(false),
-    m(5120), n(4096), k(4096), l(1), iterations(20), verify(1), 
-    alpha(1.f), beta(0.f)
-  { }
-  void parse(int argc, char const **args) {
-    cutlass::CommandLine cmd(argc, args);
-    if (cmd.check_cmd_line_flag("help")) {
-      help = true;
-      return;
-    }
-    cmd.get_cmd_line_argument("m", m, 5120);
-    cmd.get_cmd_line_argument("n", n, 4096);
-    cmd.get_cmd_line_argument("k", k, 4096);
-    cmd.get_cmd_line_argument("l", l, 1);
-    cmd.get_cmd_line_argument("alpha", alpha, 1.f);
-    cmd.get_cmd_line_argument("beta", beta, 0.f);
-    cmd.get_cmd_line_argument("iterations", iterations, 100);
-    cmd.get_cmd_line_argument("verify", verify, 1);
-  }
-  std::ostream & print_usage(std::ostream &out) const {
-    out << "BMG GEMM Example\n\n"
-      << "Options:\n\n"
-      << "  --help                      If specified, displays this usage statement\n\n"
-      << "  --m=<int>                   Sets the M extent of the GEMM\n"
-      << "  --n=<int>                   Sets the N extent of the GEMM\n"
-      << "  --k=<int>                   Sets the K extent of the GEMM\n"
-      << "  --l=<int>                   Sets the L extent (batch count) of the GEMM\n"
-      << "  --alpha=<s32>               Epilogue scalar alpha\n"
-      << "  --beta=<s32>                Epilogue scalar beta\n\n"
-      << "  --iterations=<int>          Iterations\n\n"
-      << "  --verify=<int>              Specify whether to verify.\n\n";
-    return out;
-  }
-};
-template <
-  class Gemm
->
-struct ExampleRunner {
-  using StrideA = typename Gemm::GemmKernel::StrideA;
-  using StrideB = typename Gemm::GemmKernel::StrideB;
-  using StrideC = typename Gemm::GemmKernel::StrideC;
-  using StrideD = typename Gemm::GemmKernel::StrideD;
-  using LayoutA = typename Gemm::LayoutA;
-  using LayoutB = typename Gemm::LayoutB;
-  using LayoutC = typename Gemm::LayoutC;
-  using LayoutD = typename Gemm::LayoutD;
-  using ElementA = typename Gemm::ElementA;
-  using ElementB = typename Gemm::ElementB;
-  using ElementAccumulator = typename Gemm::ElementAccumulator;
-  using CollectiveEpilogue = typename Gemm::CollectiveEpilogue;
-  using ElementC = typename Gemm::ElementC;
-  using ElementOutput = typename CollectiveEpilogue::ElementOutput;
-  using ElementCompute = typename CollectiveEpilogue::ElementCompute;
-  using ProblemShapeType = typename Gemm::GemmKernel::ProblemShape;
-  StrideA stride_A;
-  StrideB stride_B;
-  StrideC stride_C;
-  StrideD stride_D;
-  uint64_t seed = 0;
-  cutlass::DeviceAllocation<ElementA> block_A;
-  cutlass::DeviceAllocation<ElementB> block_B;
-  cutlass::DeviceAllocation<ElementC> block_C;
-  cutlass::DeviceAllocation<ElementOutput> block_D;
-  cutlass::DeviceAllocation<ElementOutput> block_ref_D; // Reference GEMM result for verification
-  bool verify(const ProblemShapeType& problem_size, ElementCompute alpha, ElementCompute beta) {
-    auto [M, N, K, L] = problem_size;
-    cutlass::TensorRef ref_A(block_A.get(), LayoutA::packed({M, K}));
-    cutlass::TensorRef ref_B(block_B.get(), LayoutB::packed({K, N}));
-    cutlass::TensorRef ref_C(block_C.get(), LayoutC::packed({M, N}));
-    cutlass::TensorRef ref_D(block_ref_D.get(), LayoutD::packed({M, N}));
-    cutlass::reference::device::GemmComplex(
-          {M, N, K},
-          alpha,
-          ref_A,
-          cutlass::ComplexTransform::kNone,
-          ref_B,
-          cutlass::ComplexTransform::kNone,
-          beta,
-          ref_C,
-          ref_D,
-          ElementAccumulator(0),
-          L,     // batch_count
-          M * K, // batch_stride_A
-          K * N, // batch_stride_B
-          M * N, // batch_stride_C
-          M * N  // batch_stride_D
-        );
-    compat::wait();
-    bool passed = cutlass::reference::device::BlockCompareEqual(
-      block_ref_D.get(), block_D.get(), block_D.size());
-    return passed;
-  }
-  void initialize(const ProblemShapeType& problem_size) {
-    auto problem_shape_MNKL = cute::append<4>(problem_size, 1);
-    auto [M, N, K, L] = problem_shape_MNKL;
-    stride_A = cutlass::make_cute_packed_stride(StrideA{}, cute::make_shape(M, K, L));
-    stride_B = cutlass::make_cute_packed_stride(StrideB{}, cute::make_shape(N, K, L));
-    stride_C = cutlass::make_cute_packed_stride(StrideC{}, cute::make_shape(M, N, L));
-    stride_D = cutlass::make_cute_packed_stride(StrideD{}, cute::make_shape(M, N, L));
-    block_A.reset(static_cast<std::size_t>(M) * K * L);
-    block_B.reset(static_cast<std::size_t>(K) * N * L);
-    block_C.reset(static_cast<std::size_t>(M) * N * L);
-    block_D.reset(static_cast<std::size_t>(M) * N * L);
-    block_ref_D.reset(static_cast<std::size_t>(M) * N * L);
-    initialize_block(block_A, seed + 2023);
-    initialize_block(block_B, seed + 2022);
-    initialize_block(block_C, seed + 2021);
-  }
-  cutlass::Status run(const Options& options, const cutlass::KernelHardwareInfo& hw_info) {
-    ProblemShapeType problem_size = ProblemShapeType{options.m, options.n, options.k, options.l};
-    initialize(problem_size);
-    typename Gemm::GemmKernel::Arguments arguments{
-      cutlass::gemm::GemmUniversalMode::kGemm,
-      problem_size,
-      {block_A.get(), stride_A, block_B.get(), stride_B},
-      {{options.alpha, options.beta}, block_C.get(), stride_C, block_D.get(), stride_D},
-      hw_info
-    };
-    Gemm gemm_op;
-    size_t workspace_size = Gemm::get_workspace_size(arguments);
-    cutlass::device_memory::allocation<uint8_t> workspace(workspace_size);
-    if (gemm_op.can_implement(arguments) != cutlass::Status::kSuccess){
-      std::cout << "Invalid Problem Size: " << options.m << 'x' << options.n << 'x' << options.k << 'x' << options.l << std::endl;
-      std::exit(1);
-    }
-    CUTLASS_CHECK(gemm_op.initialize(arguments, workspace.get()));
-    CUTLASS_CHECK(gemm_op.run());
-    compat::wait();
-    if (options.verify != 0) {
-      bool passed = verify(problem_size, options.alpha, options.beta);
-      std::cout << "Disposition: " << (passed ? "Passed" : "Failed") << std::endl;
-      if (!passed) return cutlass::Status::kErrorInternal;
-    } else {
-      std::cout << "Disposition is skipped." << std::endl;
-    }
-    if (options.iterations > 0) {
-      GPU_Clock timer;
-      timer.start();
-      for (int i = 0; i < options.iterations; ++i) {
-        gemm_op.run();
-      }
-      compat::wait();
-      float cute_time = timer.seconds() / options.iterations;
-      double tflops = (2.0 * options.m * options.n * options.k * options.l) * 1e-12;
-      std::cout << "Problem Size: " << options.m << 'x' << options.n << 'x' << options.k << 'x' << options.l << std::endl;
-      printf("Cutlass GEMM Performance:     [%4.3f]TFlop/s  (%6.4f)ms\n", tflops / cute_time, cute_time*1000);
-    }
-    return cutlass::Status::kSuccess;
-  }
-};
-int main(int argc, const char** argv)
-{
+/*! \file
+    \brief Flash Attention V2 Prefill for Intel BMG
+    从sycl-tla/example/06_bmg_flash_attention修改而来
+    To run this example:
+      $ ./bin/test --seq_len_qo=512 --seq_len_kv=512 --head_size_vo=128 --head_size_qk=128
+*/
+#include "xe_fmha_fwd_runner.hpp"
+int main(int argc, const char **argv) {
   Options options;
   options.parse(argc, argv);
   if (options.help) {
@@ -202,59 +16,107 @@ int main(int argc, const char** argv)
     std::cerr << "Aborting execution." << std::endl;
     return -1;
   }
-  cutlass::KernelHardwareInfo hw_info;
-  hw_info.sm_count = cutlass::KernelHardwareInfo::query_device_multiprocessor_count(hw_info.device_id);
-  bool passed;
-  using ElementAccumulator = float;      // <- data type of accumulator
-  using ElementComputeEpilogue = float;  // <- data type of epilogue operations
-  using ElementInputA = bfloat16_t;      // <- data type of elements in input matrix A
-  using ElementInputB = bfloat16_t;      // <- data type of elements in input matrix B
-  using ElementOutput = float;           // <- data type of elements in output matrix D
-  using LayoutA = cutlass::layout::RowMajor;
-  using LayoutB = cutlass::layout::RowMajor;
-  using LayoutC = cutlass::layout::RowMajor;
-  using LayoutD = cutlass::layout::RowMajor;
-  using GmemTiledCopyA = void; //XE_LOAD_2D<16, 32, 32>;
-  using GmemTiledCopyB = void; //XE_LOAD_2D_VNNI<16, 32, 32>;
-  using TileShape = Shape<_256, _256, _32>;
-  using TiledMma = typename TiledMMAHelper<MMA_Atom<XE_DPAS_TT<8, float, cute::bfloat16_t>>, Layout<TileShape>, Layout<Shape<_8, _4, _1>, Stride<_4, _1, _0>>>::TiledMMA;
+  // Define the work-group tile shape depending on the head-size of the second matmul
+#ifdef PREFILL
+#pragma message("\033[41mPREFILL\033[0m")
+#if HEAD_DIM == 16
+  /* Tiny config for testing */
+  using ShapeQK = Shape<_1, _16, _16>;       // (q,k,d)
+  using ShapePV = Shape<_1, _16, _16>;       // (q,v,k)
+  using ShapeOut = Shape<_1, _16>;           // (q,v)
+  using SubgroupLayoutQK = Layout<Shape<_1, _1, _1>>;
+#elif HEAD_DIM == 64
+  using ShapeQK = Shape<_128, _64, _32>;
+  using ShapePV = Shape<_128, _32, _64>;
+  using ShapeOut = Shape<_128, _64>;
+  using SubgroupLayoutQK = Layout<Shape<_8, _1, _1>>;
+#elif HEAD_DIM == 96
+  using ShapeQK = Shape<_128, _64, _32>;
+  using ShapePV = Shape<_128, _32, _64>;
+  using ShapeOut = Shape<_128, _96>;
+  using SubgroupLayoutQK = Layout<Shape<_8, _1, _1>>;
+#elif HEAD_DIM == 128
+  using ShapeQK = Shape<_256, _32, _32>;
+  using ShapePV = Shape<_256, _32, _32>;
+  using ShapeOut = Shape<_256, _128>;
+  using SubgroupLayoutQK = Layout<Shape<_16, _1, _1>>;
+#elif HEAD_DIM == 192
+  using ShapeQK = Shape<_256, _64, _32>;
+  using ShapePV = Shape<_256, _32, _64>;
+  using ShapeOut = Shape<_256, _192>;
+  using SubgroupLayoutQK = Layout<Shape<_32, _1, _1>>;
+#endif
+#elif defined(DECODE)
+#pragma message("\033[42mDECODE\033[0m")
+#if PERSISTENT
+#define NUM_SG _16
+#define KV_TILE_SIZE _256
+#else
+#define NUM_SG _8
+#define KV_TILE_SIZE _512
+#endif
+#if HEAD_DIM == 16
+  /* Tiny config for testing */
+  using ShapeQK = Shape<_1, _16, _16>;       // (q,k,d)
+  using ShapePV = Shape<_1, _16, _16>;       // (q,v,k)
+  using ShapeOut = Shape<_1, _16>;           // (q,v)
+  using SubgroupLayoutQK = Layout<Shape<_1, NUM_SG, _1>>;
+#elif HEAD_DIM == 64
+    using ShapeQK = Shape<_1, KV_TILE_SIZE, _64>;
+    using ShapePV = Shape<_1, _32, KV_TILE_SIZE>;
+    using ShapeOut = Shape<_1, _64>;
+    using SubgroupLayoutQK = Layout<Shape<_1, NUM_SG, _1>>;
+#elif HEAD_DIM == 96
+    using ShapeQK = Shape<_1, KV_TILE_SIZE, _64>;
+    using ShapePV = Shape<_1, _32, KV_TILE_SIZE>;
+    using ShapeOut = Shape<_1, _96>;
+    using SubgroupLayoutQK = Layout<Shape<_1, NUM_SG, _1>>;
+#elif HEAD_DIM == 128
+    using ShapeQK = Shape<_1, KV_TILE_SIZE, _64>;
+    using ShapePV = Shape<_1, _32, KV_TILE_SIZE>;
+    using ShapeOut = Shape<_1, _128>;
+    using SubgroupLayoutQK = Layout<Shape<_1, NUM_SG, _1>>;
+#elif HEAD_DIM == 192
+    using ShapeQK = Shape<_1, KV_TILE_SIZE, _64>;
+    using ShapePV = Shape<_1, _32, KV_TILE_SIZE>;
+    using ShapeOut = Shape<_1, _192>;
+    using SubgroupLayoutQK = Layout<Shape<_1, NUM_SG, _1>>;
+#endif
+#else
+#error Either DECODE or PREFILL should be defined.
+#endif
+#ifdef DECODE
+  constexpr int PipelineStages = 1;
+#else
   constexpr int PipelineStages = 2;
-  using GEMMDispatchPolicy = cutlass::gemm::MainloopXeL1Staged<PipelineStages>;
-  using EpilogueDispatchPolicy = cutlass::epilogue::IntelXeGeneric;
-  using EpilogueOp = cutlass::epilogue::fusion::LinearCombination<ElementOutput, ElementComputeEpilogue, ElementAccumulator, ElementAccumulator, cutlass::FloatRoundStyle::round_to_nearest>;
-  using FusionCallbacks = cutlass::epilogue::fusion::FusionCallbacks<EpilogueDispatchPolicy, EpilogueOp, TileShape, decltype(tile_shape(TiledMma()))>;
-
-  using CollectiveEpilogue = cutlass::epilogue::collective::CollectiveEpilogue<
-          EpilogueDispatchPolicy,
-          TileShape,
-          void,                                   // Epilogue tile (void = automatic)
-          ElementAccumulator,
-          cutlass::gemm::TagToStrideC_t<LayoutC>, // Converts CUTLASS 2.x to CUTLASS 3.x representation
-          ElementOutput,
-          cutlass::gemm::TagToStrideC_t<LayoutD>, // Converts CUTLASS 2.x to CUTLASS 3.x representation
-          FusionCallbacks,
-          void,                                   // The copy atom used to load matrix C  (void = automatic)
-          void>;                                  // The copy atom used to store matrix D (void = automatic)
-
-  using CollectiveMainloop = cutlass::gemm::collective::CollectiveMma<
-          GEMMDispatchPolicy,
-          TileShape,
-          ElementInputA,
-          cutlass::gemm::TagToStrideA_t<LayoutA>, // Converts CUTLASS 2.x to CUTLASS 3.x representation
-          ElementInputB,
-          cutlass::gemm::TagToStrideB_t<LayoutB>, // Converts CUTLASS 2.x to CUTLASS 3.x representation
-          TiledMma,
-          GmemTiledCopyA, void, void, cute::identity,  // A
-          GmemTiledCopyB, void, void, cute::identity   // B
-  >;
-
-  using GemmKernel = cutlass::gemm::kernel::GemmUniversal<
-          Shape<int, int, int, int>, // Defer global problem shape definition to runtime
-          CollectiveMainloop,
-          CollectiveEpilogue
-  >;
-  using Gemm = cutlass::gemm::device::GemmUniversalAdapter<GemmKernel>;
-  ExampleRunner<Gemm> runner;
-  CUTLASS_CHECK(runner.run(options, hw_info));
-  return 0;
+#endif
+#ifdef IS_FLOAT_E5M2
+  using ElementQ = cutlass::float_e5m2_t;
+  using ElementK = cutlass::float_e5m2_t;
+  using ElementV = cutlass::float_e5m2_t;
+#elif defined(IS_FLOAT_E4M3)
+  using ElementQ = cutlass::float_e4m3_t;
+  using ElementK = cutlass::float_e4m3_t;
+  using ElementV = cutlass::float_e4m3_t;
+#else
+  using ElementQ = bfloat16_t;
+  using ElementK = bfloat16_t;
+  using ElementV = bfloat16_t;
+#endif
+#if PERSISTENT
+  return FMHAConfig<false/*isCausal*/, 
+		    ShapeQK/*TileShapeQK*/, 
+		    ShapePV/*TileShapePV*/, 
+		    ShapeOut/*TileShapeOutput*/, 
+		    SubgroupLayoutQK/*SgLayoutQK*/, 
+		    void,/*SgLayoutPV*/ 
+		    PipelineStages, 
+		    true/*isPersistent*/, 
+		    ElementQ/*Datatype Q*/, 
+		    ElementK/*DataType K*/, 
+		    ElementV/*DataType V*/>::run(options);
+#else
+  return options.is_causal ? FMHAConfig<true, ShapeQK, ShapePV, ShapeOut, SubgroupLayoutQK, void, PipelineStages,  /*persistent=*/false, ElementQ, ElementK, ElementV>::run(options)
+  : FMHAConfig<false, ShapeQK, ShapePV, ShapeOut, SubgroupLayoutQK, void, PipelineStages,  /*persistent=*/false, ElementQ, ElementK, ElementV>::run(options);
+#endif
 }
